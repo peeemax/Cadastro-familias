213414d75ea693878ec1d753cf34a01501174663
Pedro Maximiano <97562548+peeemax@users.noreply.github.com>
2022-07-25 22:14:36 -0300

    Criada a Classe Familia e seus atributos.

--- cadastro_familia.py
+++ cadastro_familia.py
@@ -1,4 +1,14 @@
-def cadastro_familia:
-    familia = ()
-
-    input('')
+class Familia:
+    '''Classe para chamar e registrar a familia'''
+    def __init__(self, nome, endereco, qtd_membros, membros): # Método construtor da classe
+        self.nome = nome
+        self.endereco = endereco
+        self.qtd_membros = qtd_membros
+        self.membros = membros
+    
+    def membros(self):
+        membros = list(Pessoa)
+    
+if __name__ == "__main__":
+    familia1 = Familia('Maximiano', 'Campinas-SP', '3')
+    print(familia1)
--- menu.py
+++ menu.py
@@ -1,6 +1,9 @@
 
 
 # Função de menu de acesso às funções do software:
+from cadastro_familia import Familia
+
+
 def exibir_menu():
     opcao = int(input('''Digite o número de sua opção para acessar:
     1 - Cadastrar uma família
@@ -9,8 +12,10 @@ def exibir_menu():
     '''))
 
     if opcao == 1:
-        print('Cadastrar')
+        Familia(input())
     else:
         print('errado')
 
 exibir_menu()
+
+
