90ed77ab96482ba9e3f918775f420b208194b322
Pedro Maximiano <97562548+peeemax@users.noreply.github.com>
2022-07-26 23:46:15 -0300

    Criada classe do objeto Pessoa para ser inserida dentro da classe Famili

--- cadastro_familia.py
+++ cadastro_familia.py
@@ -1,3 +1,6 @@
+from types import prepare_class
+
+
 class Familia:
     '''Classe para chamar e registrar a familia'''
     def __init__(self, nome, endereco, qtd_membros, membros): # Método construtor da classe
@@ -12,3 +15,11 @@ class Familia:
 if __name__ == "__main__":
     familia1 = Familia('Maximiano', 'Campinas-SP', '3')
     print(familia1)
+
+
+class Pessoa:
+    def __init__(self, nome, data_nascimento, cpf, idade):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.idade = idade
