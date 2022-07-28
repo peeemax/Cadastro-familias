a653e82cff89db28d8949f97095a97bf7e20e010
peeemax <peeemax@hotmail.com>
2022-07-27 21:14:52 -0300

    Criado a lógica para o menu. Feito as lógicas para cadastro da familia.

--- cadastro_familia.py
+++ cadastro_familia.py
@@ -1,25 +1,34 @@
 from types import prepare_class
 
 
+
+
+
 class Familia:
     '''Classe para chamar e registrar a familia'''
-    def __init__(self, nome, endereco, qtd_membros, membros): # Método construtor da classe
+
+    def __init__(self, nome, endereco, qtd_membros, membros):  # Método construtor da classe
         self.nome = nome
         self.endereco = endereco
         self.qtd_membros = qtd_membros
-        self.membros = membros
-    
-    def membros(self):
-        membros = list(Pessoa)
-    
+        self.membros = Pessoa
+
+
+
 if __name__ == "__main__":
-    familia1 = Familia('Maximiano', 'Campinas-SP', '3')
+    familia1 = Familia('Maximiano', 'Campinas-SP', '3', '')
     print(familia1)
+    print(familia1.nome)
 
 
 class Pessoa:
-    def __init__(self, nome, data_nascimento, cpf, idade):
+    def __init__(self, nome, data_nascimento, idade, cpf):
         self.nome = nome
         self.data_nascimento = data_nascimento
         self.cpf = cpf
         self.idade = idade
+
+if __name__ == "__main__":
+    pessoa1 = Pessoa('Pedro Maximiano', '13/09/1992', '29 anos', '42511753871')
+    print(pessoa1)
+    print(pessoa1.idade)
--- menu.py
+++ menu.py
@@ -1,7 +1,5 @@
-
-
 # Função de menu de acesso às funções do software:
-from cadastro_familia import Familia
+from cadastro_familia import Familia, Pessoa
 
 
 def exibir_menu():
@@ -11,11 +9,59 @@ def exibir_menu():
     3 - Procurar dados de uma familia
     '''))
 
-    if opcao == 1:
-        Familia(input())
-    else:
-        print('errado')
+    while opcao == 1 or 2 or 3:
+
+        if opcao == 1:
+
+            deseja_novo_cadastro = 'Sim'
+
+            while deseja_novo_cadastro == 'Sim':
+
+                familia1 = Familia(input('Digite o sobrenome da familia: '),
+                                   input('Digite o Endereço da residência, com a cidade e estado separado por (-):'),
+                                   input('Digite a quantidade de membros da familia:'),
+                                   (Pessoa(input('Digite o nome completo do membro?'),
+                                          input('Digite a data de nascimento. (DD/MM/AAAA)'),
+                                          input('Digite a idade (somente numeros).'),
+                                          input('Digite o CPF. (somente numeros)'))))
+
+                print(familia1.nome, familia1.endereco, familia1.qtd_membros, familia1.membros)
+
+                deseja_novo_cadastro = input('Deseja fazer um novo cadastro de familia? (Digite Sim/Não)')
+
+                if deseja_novo_cadastro == 'Sim':
+                    familia2 = Familia(input('Digite o sobrenome da familia: '),
+                                       input(
+                                           'Digite o Endereço da residência, com a cidade e estado separado por (-):'),
+                                       input('Digite a quantidade de membros da familia:'),
+                                       input('Digite o nome dos membros, separado por vírgula: '))
+
+                    print(familia2.nome, familia2.endereco, familia2.qtd_membros, familia2.membros)
+
+                    deseja_novo_cadastro = input('Deseja fazer um novo cadastro de familia? (Digite Sim/Não)')
+
+                    print('Obrigado')
+
+        elif opcao == 2:
+
+            print(familia1.nome, familia1.endereco, familia1.qtd_membros, familia1.membros)
+
+            print(familia2.nome, familia2.endereco, familia2.qtd_membros, familia2.membros)
+
+
+        elif opcao == 3:
+
+            print('Fazer a busca')
+
+        opcao = int(input('''Digite o número de sua opção para acessar:
+            1 - Cadastrar uma família
+            2 - Listar familias cadastradas
+            3 - Procurar dados de uma familia
+            4 - Sair
+            '''))
+
+        if opcao == 4:
+            break
+
 
 exibir_menu()
-
-
